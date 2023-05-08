7f24afe970e19af9151f3419ed8d80037f915633
Petr Pamuzhak <petromars444@gmail.com>
2023-05-08 14:45:31 +0300

    v.2.0

--- src/components/Email/Email.js
+++ src/components/Email/Email.js
@@ -17,20 +17,8 @@ export const StyledSocialList = styled.ul`
     display: flex;
   }
 
-  &:after {
-    content: '';
-    display: block;
-    width: 1px;
-    height: 90px;
-    margin: 0 auto;
-    background-color: var(--light-slate);
-    
-  }
-
+  
   li {
-    &:last-of-type {
-      margin-bottom: 20px;
-    }
 
     a {
       padding: 10px;
